b57b9d3ea54efa56ea704e0c26ba394e9d2da487
charles-lang <charles.lang@nyu.edu>
2017-07-25 13:31:14 -0400

    Change filepath for csv

--- Unit 2 - Data Sources & Manipulation/Looking_at_Data/initLesson.R
+++ Unit 2 - Data Sources & Manipulation/Looking_at_Data/initLesson.R
@@ -7,7 +7,7 @@
 
 # Path to data
 .datapath <- file.path(.get_course_path(),
-                      'R_Programming', 'Looking_at_Data',
+                      'Unit 2 - Data Sources & Manipulation', 'Looking_at_Data',
                       'plant-data.txt')
 # Read in data
 plants <- read.csv(.datapath, strip.white=TRUE, na.strings="")
