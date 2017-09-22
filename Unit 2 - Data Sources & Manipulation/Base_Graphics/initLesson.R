dceea46739af578bf4ee4dbebdc09cc7c3f7114d
charles-lang <charles.lang@nyu.edu>
2017-09-22 14:06:39 -0400

    Remove file path to plant-data.txt

--- Unit 2 - Data Sources & Manipulation/Base_Graphics/initLesson.R
+++ Unit 2 - Data Sources & Manipulation/Base_Graphics/initLesson.R
@@ -5,21 +5,6 @@
   )
 }
 
-# Path to data
-.datapath <- file.path(.get_course_path(),
-                      'Data Sources & Manipulation', 'Base_Graphics',
-                      'plant-data.txt')
-# Read in data
-plants <- read.csv(.datapath, strip.white=TRUE, na.strings="")
 
-# Remove annoying columns
-.cols2rm <- c('Accepted.Symbol', 'Synonym.Symbol')
-plants <- plants[, !(names(plants) %in% .cols2rm)]
-
-# Make names pretty
-names(plants) <- c('Scientific_Name', 'Duration', 'Active_Growth_Period',
-                   'Foliage_Color', 'pH_Min', 'pH_Max',
-                   'Precip_Min', 'Precip_Max',
-                   'Shade_Tolerance', 'Temp_Min_F')
 
 swirl_options(swirl_logging = TRUE)
