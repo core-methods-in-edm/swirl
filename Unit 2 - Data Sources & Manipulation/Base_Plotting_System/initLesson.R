3998554e76519539fb1ab05f0df2ef5cef57d3ea
charles-lang <charles.lang@nyu.edu>
2020-10-08 21:42:07 -0400

    Upload three more units

---
+++ Unit 2 - Data Sources & Manipulation/Base_Plotting_System/initLesson.R
@@ -0,0 +1,20 @@
+library(jpeg)
+library(datasets)
+
+# For compatibility with 2.2.21
+.get_course_path <- function(){
+  tryCatch(swirl:::swirl_courses_dir(),
+           error = function(c) {file.path(find.package("swirl"),"Courses")}
+  )
+}
+
+data(cars)
+data(mpg)
+state <- data.frame(state.x77, region = state.region)
+# Put initialization code in this file.
+path_to_course <- file.path(.get_course_path(),
+	"Unit 3 - Structure Discovery","Base_Plotting_System")
+try(dev.off(), silent=TRUE)
+plot.new()
+
+swirl_options(swirl_logging = TRUE)
---
+++ Unit 2 - Data Sources & Manipulation/Principles_of_Analytic_Graphs/initLesson.R
@@ -0,0 +1,16 @@
+library(ggplot2)
+library(jpeg)
+
+# For compatibility with 2.2.21
+.get_course_path <- function(){
+  tryCatch(swirl:::swirl_courses_dir(),
+           error = function(c) {file.path(find.package("swirl"),"Courses")}
+  )
+}
+
+# Put initialization code in this file.
+path_to_course <- file.path(.get_course_path(),
+	"Unit 3 - Structure Discovery","Principles_of_Analytic_Graphs")
+plot.new()
+
+swirl_options(swirl_logging = TRUE)
